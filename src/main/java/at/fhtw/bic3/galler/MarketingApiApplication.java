6e99226d2f0c6aaf1e69b991611f9aee069305c3
Thomas Galler <thomas.galler@snt.at>
2022-12-21 18:00:54 +0100

    Initial Commit

---
+++ src/main/java/at/fhtw/bic3/galler/MarketingApiApplication.java
@@ -0,0 +1,13 @@
+package at.fhtw.bic3.galler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketingApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarketingApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/at/fhtw/bic3/galler/MarketingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package at.fhtw.bic3.galler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketingApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
